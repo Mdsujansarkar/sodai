40dfd7557de09226df89ab1da673b228394abb57
Mdsujansarkar <sujansarkar786@gmail.com>
2022-10-23 23:14:17 +0600

    Headr and Footer separate

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/admin', [BackendController::class, 'index']);
+//Route::get('/admin', [BackendController::class, 'index']);
+Route::get('/admin', [App\Http\Controllers\Backend\BackendController::class, 'index']);
