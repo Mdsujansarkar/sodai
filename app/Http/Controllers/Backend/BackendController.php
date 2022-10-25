a4ab4f513a736bc2a9dc48411e2bed73fdcffcf5
Mdsujansarkar <sujansarkar786@gmail.com>
2022-10-26 00:16:44 +0600

    category add

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -14,7 +14,7 @@ class BackendController extends Controller
      */
     public function index()
     {
-        return view('admin.deshboard');
+        return view('admin.home.home');
     }
 
     /**
