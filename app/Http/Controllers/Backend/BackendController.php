11a860750d946f8b064cfda998fb9fbe7e3062a7
Mdsujansarkar <sujansarkar786@gmail.com>
2022-10-26 20:21:51 +0600

    debger add

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -12,7 +12,7 @@ class BackendController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): Response
     {
         return view('admin.home.home');
     }
--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -11,11 +11,11 @@
                 </div>
               </div>
               <div class="flex-auto p-6">
-                <p class="leading-normal uppercase dark:text-white dark:opacity-60 text-sm">Categories Information</p>
+                <p class="leading-normal uppercase dark:text-white dark:opacity-60 text-sm">{{__('Categories Information')}}</p>
                 <div class="flex flex-wrap -mx-3">
                   <div class="w-full max-w-full px-3 shrink-0 md:w-6/12 md:flex-0">
                     <div class="mb-4">
-                      <label for="Name" class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">Name<span style="color:red">*</span></label>
+                      <label for="Name" class="inline-block mb-2 ml-1 font-bold text-xs text-slate-700 dark:text-white/80">{{__('Category Name')}}<span style="color:red">*</span></label>
                       <input type="text" name="username" value="lucky.jesse" class="focus:shadow-primary-outline dark:bg-slate-850 dark:text-white text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none" />
                     </div>
                   </div>
